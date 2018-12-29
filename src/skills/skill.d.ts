72bdcfac1182683093522563b10dfbe5d7d78813
Alex Castillo <alex@castillo.io>
2018-12-28 21:23:04 -0700

    UPDATE: ISkill

--- src/skills/skill.d.ts
+++ src/skills/skill.d.ts
@@ -1,15 +1,11 @@
 export interface ISkill {
   id: string;
+  spec: string;
   name: string;
   description: string;
   organization?: string;
-  registry: string;
-  packageName: string;
-  version: string;
   metrics: string[];
-  rating?: number;
   userId: string;
-  publishedDate: number;
-  updatedDate?: number;
+  timestamp: number;
   status: string;
 }
