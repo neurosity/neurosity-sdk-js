d499692c4712e09a847aa3cf77ec7d1cffd4a807
Alex Castillo <alex@castillo.io>
2019-12-26 10:31:52 -0700

    FEAT: add sleepMode typings to DeviceStatus

--- src/types/status.ts
+++ src/types/status.ts
@@ -2,6 +2,8 @@ export interface DeviceStatus {
   battery: number;
   charging: boolean;
   state: "online" | "offline" | "updating" | "booting" | "shuttingOff";
+  sleepMode: boolean;
+  sleepModeReason: "updating" | "charging" | null;
   updatingProgress: number;
   ssid: string;
 }
