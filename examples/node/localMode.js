91549b65754f84d2d9dead261bd77354d1712e30
Alex Castillo <alex@castillo.io>
2020-04-15 12:56:31 -0400

    RFR: renamed to local mode + added support to dynamic local mode
    DOC: documented local mode + fixed some doc typos

--- examples/node/localMode.js
+++ examples/node/localMode.js
@@ -1,7 +1,7 @@
 module.exports = async function (notion) {
   await notion
-    .enableIncognitoMode(true)
-    .catch((error) => console.log(error.message));
+    .enableLocalMode(true)
+    .catch((error) => console.log(error));
 
   notion.brainwaves("powerByBand").subscribe((powerByBand) => {
     console.log("powerByBand", powerByBand.data);
