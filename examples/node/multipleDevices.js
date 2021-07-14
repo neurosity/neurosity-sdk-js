12d129cb77bdc3ac801307b42e10fa2b079a6160
Alex Castillo <alex@castillo.io>
2021-07-14 12:21:19 -0700

    ADD: example for streaming from multiple devices

---
+++ examples/node/multipleDevices.js
@@ -0,0 +1,44 @@
+const { Notion } = require("../..");
+const { empty } = require("rxjs");
+const { switchMap, map } = require("rxjs/operators");
+
+// Note: when `deviceId` is not passed, and `autoSelectDevice`
+// is set to false, the `selectDevice method should be called`
+const notion = new Notion({
+  autoSelectDevice: false
+});
+
+(async () => {
+  await notion
+    .login({
+      email: process.env.NEUROSITY_EMAIL,
+      password: process.env.NEUROSITY_PASSWORD
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+
+  const devices = await notion.getDevices().catch(console.error);
+
+  devices.forEach(async (device) => {
+    const { deviceId } = device;
+    const deviceInstance = new Notion({ deviceId });
+
+    deviceInstance
+      .onAuthStateChanged()
+      .pipe(
+        switchMap((user) =>
+          user
+            ? deviceInstance.status().pipe(
+                map((status) => ({
+                  [deviceId]: status
+                }))
+              )
+            : empty()
+        )
+      )
+      .subscribe((status) => {
+        console.log(status);
+      });
+  });
+})();
