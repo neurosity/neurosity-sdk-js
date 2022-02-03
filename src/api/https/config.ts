d6897431a791912a39787fb7dc9f307187c19da6
Alex Castillo <alex@neurosity.co>
2022-02-02 16:23:49 -0800

    FEAT: use vanity url for all cloud functions

--- src/api/https/config.ts
+++ src/api/https/config.ts
@@ -1,4 +1,1 @@
-export const prodFunctionsBaseUrl =
-  "https://us-central1-neurosity-device.cloudfunctions.net";
-
-export const prodFunctionsVanityUrl = "https://api.neurosity.co";
+export const prodFunctionsBaseUrl = "https://api.neurosity.co";
