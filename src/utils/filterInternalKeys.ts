25523da387e1ec29d7925b9164b131c6732bda4b
Alex Castillo <alex@neurosity.co>
2022-02-24 11:45:17 -0800

    FEAT: add client-side hearbeat detection

---
+++ src/utils/filterInternalKeys.ts
@@ -0,0 +1,26 @@
+import { MonoTypeOperatorFunction, pipe } from "rxjs";
+import { map } from "rxjs/operators";
+import { DeviceStatus } from "../types/status";
+
+export function filterInternalKeys(): MonoTypeOperatorFunction<DeviceStatus> {
+  return pipe(
+    map((status): DeviceStatus => {
+      if (!status) {
+        return status;
+      }
+
+      // remove internal properties that start with "__"
+      const filteredStatus: any = Object.entries(status).reduce(
+        (acc, [key, value]) => {
+          if (!key.startsWith("__")) {
+            acc[key] = value;
+          }
+          return acc;
+        },
+        {}
+      );
+
+      return filteredStatus;
+    })
+  );
+}
