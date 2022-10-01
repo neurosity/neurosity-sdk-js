73e5558a2bca92317ce2d54269bd77e757ce9494
Alex Castillo <alex@neurosity.co>
2022-10-01 16:37:21 -0700

    FEAT: ble -> added react native max byte size for write requests

--- src/api/bluetooth/constants.ts
+++ src/api/bluetooth/constants.ts
@@ -1,7 +1,9 @@
 import { BLUETOOTH_CHARACTERISTICS } from "@neurosity/ipk";
 
-export const ANDROD_MAX_MTU = 512;
-export const DEFAULT_ACTION_RESPONSE_TIMEOUT = 1000 * 60; // 1 minute
+export const ANDROID_MAX_MTU: number = 512;
+export const REACT_NATIVE_MAX_BYTE_SIZE: number = 512;
+
+export const DEFAULT_ACTION_RESPONSE_TIMEOUT: number = 1000 * 60; // 1 minute
 
 // Reverse BLUETOOTH_CHARACTERISTICS key/values for easy lookup
 export const CHARACTERISTIC_UUIDS_TO_NAMES = Object.fromEntries(
