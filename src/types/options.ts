f924db87355af4b3e2d47bc15b104f5dc9ba08b6
Alex Castillo <alex@neurosity.co>
2022-12-04 12:37:38 -0800

    FEAT: ble pass all deps to the bluetooth client

--- src/types/options.ts
+++ src/types/options.ts
@@ -4,6 +4,7 @@ import { BluetoothTransport } from "../api/bluetooth/BluetoothClient";
 
 export enum STREAMING_MODE {
   CLOUD_ONLY = "cloud-only",
+  BLUETOOTH_ONLY = "bluetooth-only",
   CLOUD_WITH_BLUETOOTH_FALLBACK = "cloud-with-bluetooth-fallback",
   BLUETOOTH_WITH_CLOUD_FALLBACK = "bluetooth-with-cloud-fallback"
 }
