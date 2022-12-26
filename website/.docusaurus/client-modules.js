37728541eb2b1b7c88b35bf9883a55da2e71c9f3
Alex Castillo <alex@neurosity.co>
2022-12-26 13:52:48 -0800

    DOCS: remove .docusaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('/Users/alexcastillo/Code/notion-js/website/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/alexcastillo/Code/notion-js/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/alexcastillo/Code/notion-js/website/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/alexcastillo/Code/notion-js/website/src/css/customTheme.css'),
-];
