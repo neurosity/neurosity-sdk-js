32419ff2aaa1adc42ed4330c6e24332b92f5c558
Alex Castillo <alex@neurosity.co>
2023-01-09 11:56:18 -0800

    EXPORT: ble -> types

--- src/api/bluetooth/index.ts
+++ src/api/bluetooth/index.ts
@@ -2,3 +2,4 @@ export * from "./BluetoothClient";
 export * from "./web/WebBluetoothTransport";
 export * from "./react-native/ReactNativeTransport";
 export * from "./utils/osHasBluetoothSupport";
+export * from "./types/index";
