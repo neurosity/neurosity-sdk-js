49e5ba4c3cb32d305fc90f96ffd2ebb716314c30
AJ Keller <aj@neurosity.co>
2024-12-31 23:52:48 +0000

    ADD: brainwave tests

--- src/types/brainwaves.ts
+++ src/types/brainwaves.ts
@@ -1,36 +1,51 @@
-export type BrainwavesLabel =
-  | "raw"
-  | "rawUnfiltered"
-  | "powerByBand"
-  | "psd";
+export type BrainwavesLabel = "raw" | "rawUnfiltered" | "powerByBand" | "psd";
 
 export type AmplitudeByChannel = number[][];
 
+export interface BaseInfo {
+  samplingRate: number;
+  startTime: number;
+  channelNames: string[];
+}
+
+export interface EpochInfo extends BaseInfo {
+  notchFrequency?: string;
+}
+
 export interface Epoch {
   data: AmplitudeByChannel;
-  info: {
-    samplingRate: number;
-    startTime: number;
-  };
+  info: EpochInfo;
+}
+
+export interface RawUnfilteredEpochInfo extends BaseInfo {}
+
+export interface RawUnfilteredEpoch {
+  data: AmplitudeByChannel;
+  info: RawUnfilteredEpochInfo;
 }
 
 export type PSDByChannel = number[][];
 
+export interface PSDInfo extends BaseInfo {
+  notchFrequency: string;
+}
+
 export interface PSD {
+  label: "psd";
   psd: PSDByChannel;
   freqs: number[];
-  info: {
-    samplingRate: number;
-    startTime: number;
-  };
+  info: PSDInfo;
 }
 
 export type BandName = "gamma" | "beta" | "alpha" | "theta" | "delta";
 
+// PowerByBand includes an info object with sampling rate, start time, and channel names
+// since this information is still relevant for frequency band analysis
 export interface PowerByBand {
   gamma: number[];
   beta: number[];
   alpha: number[];
   theta: number[];
   delta: number[];
+  info: BaseInfo;
 }
